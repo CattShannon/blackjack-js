fb56e3871f7cd04e5980734a0c44679179e995d1
Sebastian Aguirre <sebastianaguirre103@gmail.com>
2024-12-23 12:13:07 -0500

    refactor

--- assets/js/ia-win-threshold-calculator.js
+++ assets/js/ia-win-threshold-calculator.js
@@ -1,21 +1,29 @@
 
-const probabilities = [0.1, 0.25, 0.4, 0.15, 0.05, 0.05]// para numeros de 0 a 5
-//                      0    1     2    3     4     5
+const iaBehavior = (() => {
 
-const acummulativeDistribution = probabilities.reduce((acc, initial) => {
-    const lastDistribution =  acc[acc.length - 1] || 0;
-    acc.push(lastDistribution + initial);
+    'use strict'
 
-    return acc;
-}, []);
+    const probabilities = [0.1, 0.25, 0.4, 0.15, 0.05, 0.05]// para numeros de 0 a 5
+    //                      0    1     2    3     4     5
 
+    const acummulativeDistribution = probabilities.reduce((acc, initial) => {
+        const lastDistribution = acc[acc.length - 1] || 0;
+        acc.push(lastDistribution + initial);
 
-const getIAWinningThreshold = () => {
-    const random = Math.random();
+        return acc;
+    }, []);
 
-    for(let i in acummulativeDistribution){
-        if(random < acummulativeDistribution[i]){
-            return i * 1;
+    const getIAWinningThreshold = () => {
+        const random = Math.random();
+
+        for (let i in acummulativeDistribution) {
+            if (random < acummulativeDistribution[i]) {
+                return i * 1;
+            }
         }
     }
-}
+
+    return {
+        getIAWinningThreshold
+    };
+})();
--- assets/js/juego.js
+++ assets/js/juego.js
@@ -8,180 +8,187 @@
  * 
  * Gana el jugador que consiga acercarse mas a los 21 puntos sin pasarse de esa cantidad
  */
-const SPECIAL_CARDS = {
-    A: 11,
-    J: 10,
-    Q: 10,
-    K: 10
-};
-const WINNING_SCORE = 21;
 
-let playerScore,
-    IAScore;
-let currentlyInGame;
-let deck;
-let IAWinningThreshold;
+(() => {
+    'use strict'
+    
+    const SPECIAL_CARDS = {
+        A: 11,
+        J: 10,
+        Q: 10,
+        K: 10
+    },
+        WINNING_SCORE = 21;
 
-Object.freeze(SPECIAL_CARDS);
+    let playerScore,
+        IAScore,
+        currentlyInGame,
+        deck,
+        IAWinningThreshold;
 
-//Referencias del HTML
-const btnNewGame = document.querySelector('#btn-new-game');
-const btnTakeCard = document.querySelector('#btn-take-card');
-const btnStopGame = document.querySelector('#btn-stop-game');
-const smallPlayerScore = document.querySelector('#small-player-score');
-const smallIAScore = document.querySelector('#small-ia-score');
-const divPlayerCards = document.querySelector('#div-player-cards');
-const divIACards = document.querySelector('#div-ia-cards');
+    Object.freeze(SPECIAL_CARDS);
 
-
-const fillWithCardTypes = (cardTypes, cardValue) => {
-    for (let cartType of cardTypes) {
-        deck.push(`${cardValue}${cartType}`);
-    }
-}
-
-const createDeck = () => {
-    let cardTypes = ['C', 'D', 'H', 'S'];
-
-    deck = [];
-
-    for (let i = 2; i <= 10; i++) {
-        fillWithCardTypes(cardTypes, i)
-    }
-
-    for (let specialCard of Object.getOwnPropertyNames(SPECIAL_CARDS)) {
-        fillWithCardTypes(cardTypes, specialCard)
-    }
-
-    deck = _.shuffle(deck);
-};
-
-const startNewGame = () => {
-    console.clear();
-    divPlayerCards.innerHTML = '';
-    divIACards.innerHTML = '';
-    playerScore = 0;
-    IAScore = 0;
-    createDeck();
-    currentlyInGame = true;
-    smallPlayerScore.innerText = '0'
-    smallIAScore.innerText = '0'
-    IAWinningThreshold = getIAWinningThreshold();
-};
-
-const takeACard = () => {
-
-    if (deck.length === 0) {
-        throw 'No hay más cartas en el deck';
-    }
-
-    let card = deck.pop();
-    return card;
-};
-
-const cardValue = (card) => {
-    const value = card.match(/(\d{1,2}|\w{1})(\w+)/i).at(1);
-
-    return (isNaN(value)) ? SPECIAL_CARDS[value] : value * 1
-};
+    //Referencias del HTML
+    const btnNewGame = document.querySelector('#btn-new-game'),
+          btnTakeCard = document.querySelector('#btn-take-card'),
+          btnStopGame = document.querySelector('#btn-stop-game'),
+          smallPlayerScore = document.querySelector('#small-player-score'),
+          smallIAScore = document.querySelector('#small-ia-score'),
+          divPlayerCards = document.querySelector('#div-player-cards'),
+          divIACards = document.querySelector('#div-ia-cards');
 
 
-const executeTurn = () => {
-    const takenCard = takeACard();
-    const turnScore = cardValue(takenCard);
-
-    let newCardImageElement = document.createElement('img');
-    newCardImageElement.classList.add('blackjack-card')
-    newCardImageElement.src = `assets/cartas/${takenCard}.png`;
-
-    return {
-        turnScore,
-        newCardImageElement
-    }
-};
-
-const displayTurnResult = (divCards, smallScore, htmlNewCardImage, actualScore) => {
-    divCards.append(htmlNewCardImage);
-    smallScore.innerText = `${actualScore}`
-}
-
-
-const playIATurn = () => {
-
-    const scoringGoal = WINNING_SCORE - IAWinningThreshold;
-    let IAStillPlaying = true;
-    let temporaryIAScore = 0;
-
-    console.warn('Stating IA turn');
-
-    do {
-
-        let turnResult = executeTurn(IAScore);
-        temporaryIAScore += turnResult.turnScore;
-
-        if (temporaryIAScore <= scoringGoal) {
-
-            IAScore = temporaryIAScore;
-            displayTurnResult(divIACards, smallIAScore, turnResult.newCardImageElement, IAScore);
-
-            if (IAScore === scoringGoal || IAScore === scoringGoal - 1) {
-                IAStillPlaying = false;
-            }
-
-            console.warn("La computadora está tomando otra carta.");
-        } else {
-            temporaryIAScore = IAScore;
+    const fillWithCardTypes = (cardTypes, cardValue) => {
+        for (let cartType of cardTypes) {
+            deck.push(`${cardValue}${cartType}`);
         }
-    } while (IAStillPlaying || deck.length < 1);
-
-    console.warn('La computadora ha finalizado su turno.')
-};
-
-const checkWinner = () => {
-    const finalPlayerScore = WINNING_SCORE - playerScore;
-    const finalIAScore = WINNING_SCORE - IAScore;
-
-    setTimeout(() => {
-        (finalPlayerScore < 0 || finalIAScore < finalPlayerScore) ? alert('Computadora gana')
-            : (finalPlayerScore === finalIAScore)
-                ? alert('Empate')
-                : alert('Has ganado');
-    }, 1000);
-};
-
-const stopPlayerTurn = () => {
-    currentlyInGame = false;
-    playIATurn();
-    checkWinner();
-};
-
-const checkPlayerTurn = (score) => (score >= WINNING_SCORE) ? stopPlayerTurn() : undefined;
-
-
-startNewGame();
-
-//Eventos
-btnNewGame.addEventListener('click', () => {
-    (!currentlyInGame || confirm('Estás seguro que deseas iniciar un juego nuevo?'))
-        && startNewGame();
-})
-
-btnTakeCard.addEventListener('click', () => {
-
-    if (!currentlyInGame) {
-        alert('No hay una partida activa');
-        return undefined;
     }
 
-    let turnResult = executeTurn();
-    playerScore += turnResult.turnScore;
+    const createDeck = () => {
+        let cardTypes = ['C', 'D', 'H', 'S'];
 
-    displayTurnResult(divPlayerCards, smallPlayerScore, turnResult.newCardImageElement, playerScore);
+        deck = [];
 
-    checkPlayerTurn(playerScore);
-})// esta funcion del segundo argumento se conoce como callback
+        for (let i = 2; i <= 10; i++) {
+            fillWithCardTypes(cardTypes, i)
+        }
 
-btnStopGame.addEventListener('click', () => {
+        for (let specialCard of Object.getOwnPropertyNames(SPECIAL_CARDS)) {
+            fillWithCardTypes(cardTypes, specialCard)
+        }
 
-    (!currentlyInGame) ? alert('No hay una partida activa') : stopPlayerTurn();
-})
+        deck = _.shuffle(deck);
+    };
+
+    const startNewGame = () => {
+
+        divPlayerCards.innerHTML = '';
+        divIACards.innerHTML = '';
+        playerScore = 0;
+        IAScore = 0;
+        createDeck();
+        currentlyInGame = true;
+        smallPlayerScore.innerText = '0'
+        smallIAScore.innerText = '0'
+        IAWinningThreshold = iaBehavior.getIAWinningThreshold();
+    };
+
+    const takeACard = () => {
+
+        if (deck.length === 0) {
+            throw 'No hay más cartas en el deck';
+        }
+
+        let card = deck.pop();
+        return card;
+    };
+
+    const cardValue = (card) => {
+        const value = card.match(/(\d{1,2}|\w{1})(\w+)/i).at(1);
+
+        return (isNaN(value)) ? SPECIAL_CARDS[value] : value * 1
+    };
+
+
+    const executeTurn = () => {
+        const takenCard = takeACard();
+        const turnScore = cardValue(takenCard);
+
+        let newCardImageElement = document.createElement('img');
+        newCardImageElement.classList.add('blackjack-card')
+        newCardImageElement.src = `assets/cartas/${takenCard}.png`;
+
+        return {
+            turnScore,
+            newCardImageElement
+        }
+    };
+
+    const displayTurnResult = (divCards, smallScore, htmlNewCardImage, actualScore) => {
+        divCards.append(htmlNewCardImage);
+        smallScore.innerText = `${actualScore}`
+    }
+
+
+    const playIATurn = () => {
+
+        const scoringGoal = WINNING_SCORE - IAWinningThreshold;
+        let IAStillPlaying = true;
+        let temporaryIAScore = 0;
+
+        console.warn('Stating IA turn');
+
+        do {
+
+            let turnResult = executeTurn(IAScore);
+            temporaryIAScore += turnResult.turnScore;
+
+            if (temporaryIAScore <= scoringGoal) {
+
+                IAScore = temporaryIAScore;
+                displayTurnResult(divIACards, smallIAScore, turnResult.newCardImageElement, IAScore);
+
+                if (IAScore === scoringGoal || IAScore === scoringGoal - 1) {
+                    IAStillPlaying = false;
+                }
+
+                console.warn("La computadora está tomando otra carta.");
+            } else {
+                temporaryIAScore = IAScore;
+            }
+        } while (IAStillPlaying || deck.length < 1);
+
+        console.warn('La computadora ha finalizado su turno.')
+    };
+
+    const checkWinner = () => {
+        const finalPlayerScore = WINNING_SCORE - playerScore;
+        const finalIAScore = WINNING_SCORE - IAScore;
+
+        setTimeout(() => {
+            (finalPlayerScore < 0 || finalIAScore < finalPlayerScore) ? alert('Computadora gana')
+                : (finalPlayerScore === finalIAScore)
+                    ? alert('Empate')
+                    : alert('Has ganado');
+        }, 1000);
+    };
+
+    const stopPlayerTurn = () => {
+        currentlyInGame = false;
+        playIATurn();
+        checkWinner();
+    };
+
+    const checkPlayerTurn = (score) => (score >= WINNING_SCORE) ? stopPlayerTurn() : undefined;
+
+
+    startNewGame();
+
+    //Eventos
+    btnNewGame.addEventListener('click', () => {
+        (!currentlyInGame || confirm('Estás seguro que deseas iniciar un juego nuevo?'))
+            && startNewGame();
+
+        console.clear();
+    })
+
+    btnTakeCard.addEventListener('click', () => {
+
+        if (!currentlyInGame) {
+            alert('No hay una partida activa');
+            return undefined;
+        }
+
+        let turnResult = executeTurn();
+        playerScore += turnResult.turnScore;
+
+        displayTurnResult(divPlayerCards, smallPlayerScore, turnResult.newCardImageElement, playerScore);
+
+        checkPlayerTurn(playerScore);
+    })// esta funcion del segundo argumento se conoce como callback
+
+    btnStopGame.addEventListener('click', () => {
+
+        (!currentlyInGame) ? alert('No hay una partida activa') : stopPlayerTurn();
+    })
+})();//Patron modulo, mas seguridad
